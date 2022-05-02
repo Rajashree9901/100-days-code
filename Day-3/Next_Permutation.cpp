cfcd495fde6495e526f122ae68171683a78a30f0
Rajashree9901 <58505906+Rajashree9901@users.noreply.github.com>
2022-05-02 17:17:09 +0530

    Update Next_Permutation.cpp

--- Day-3/Next_Permutation.cpp
+++ Day-3/Next_Permutation.cpp
@@ -1,1 +1,32 @@
+// Problem link- https://leetcode.com/problems/next-permutation/
+
+//Solution: Using built in function in cpp
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	
+	vector<int> v;
+	
+	int n;
+	cin>>n;
+	
+	for(int i=0;i<n;i++)
+	{
+	    int x;
+	    cin>>x;
+	    v.push_back(x);
+	}
+	
+  //Using Built in function "next_permutation"
+	next_permutation(v.begin(),v.end());
+	
+	for(int i=0;i<n;i++)
+	cout<<v[i]<<" ";
+	
+	return 0;
+}
 
