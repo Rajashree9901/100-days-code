3730ae8124753e61c2ad9bc19fce84f02983b77b
Rajashree9901 <58505906+Rajashree9901@users.noreply.github.com>
2022-05-04 19:16:59 +0530

    Update sort_colors.cpp

--- Day-5/sort_colors.cpp
+++ Day-5/sort_colors.cpp
@@ -1,1 +1,30 @@
+// Problem link- https://leetcode.com/problems/sort-colors/
 
+// solution 1: Using frequency
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int z=0,o=0,t=0;
+      
+//       traversing to count the frequency
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+                z++;
+            else if(nums[i]==1)
+                o++;
+            else
+                t++;
+        }
+        int i;
+      
+//       Assigning values in array at it's correct place
+        for(i=0;i<z;i++)
+            nums[i]=0;
+        for(i=z;i<z+o;i++)
+            nums[i]=1;
+        for(i=z+o;i<z+o+t;i++)
+            nums[i]=2;
+    }
+};
