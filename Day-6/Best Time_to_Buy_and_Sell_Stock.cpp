17cddbc4fd6f1cef171ddf8917b9569bd8b99a96
Rajashree9901 <58505906+Rajashree9901@users.noreply.github.com>
2022-05-05 16:19:42 +0530

    Update Best Time_to_Buy_and_Sell_Stock.cpp

--- Day-6/Best Time_to_Buy_and_Sell_Stock.cpp
+++ Day-6/Best Time_to_Buy_and_Sell_Stock.cpp
@@ -1,6 +1,6 @@
 //Problem link- https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
-// Solution: Using 2 pointers
+// Solution 1: Using 2 pointers
 
 class Solution {
 public:
@@ -20,6 +20,27 @@ public:
                 if(m<(prices[j]-prices[i]))
                     m=(prices[j]-prices[i]);
             }
+        }
+        return m;
+    }
+};
+
+// Solution 2: Maintaing minimum price and then calculating maximum profit
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        if(n==0 or n==1)
+            return 0;
+        
+        int p=INT_MAX;
+        int m=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            p=min(p,prices[i]);
+            m=max(m,prices[i]-p);
         }
         return m;
     }
