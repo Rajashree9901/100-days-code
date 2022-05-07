983b25eb76f9cb7ef56d075bad3ecdea807a4b18
Rajashree9901 <58505906+Rajashree9901@users.noreply.github.com>
2022-05-07 23:55:42 +0530

    Update MergeOverlappingSubInterval.cpp

--- Day-8/MergeOverlappingSubInterval.cpp
+++ Day-8/MergeOverlappingSubInterval.cpp
@@ -1,1 +1,40 @@
+// Problem Link- https://leetcode.com/problems/merge-intervals/
 
+// Solution
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& i) {
+        
+        sort(i.begin(),i.end());
+        
+        int l=i[0][0];
+        int r=i[0][1];
+        
+        vector<vector<int>> v;
+        
+        for(int j=1;j<i.size();j++)
+        {
+            if(r>=i[j][0])
+            {
+                if(r<i[j][1])
+                    r=i[j][1];
+            }
+            else
+            {
+                vector<int>h;
+                h.push_back(l);
+                h.push_back(r);
+                v.push_back(h);
+                l=i[j][0];
+                r=i[j][1];
+            }
+        }
+        
+        vector<int>h;
+        h.push_back(l);
+        h.push_back(r);
+        v.push_back(h);
+        return v;
+    }
+};
