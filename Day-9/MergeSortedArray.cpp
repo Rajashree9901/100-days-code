e2e15470f165d6fb3a9c7f1a95f57f31b8c8d825
Rajashree9901 <58505906+Rajashree9901@users.noreply.github.com>
2022-05-08 14:12:55 +0530

    Update MergeSortedArray.cpp

--- Day-9/MergeSortedArray.cpp
+++ Day-9/MergeSortedArray.cpp
@@ -1,6 +1,6 @@
 // Problem link- https://leetcode.com/problems/merge-sorted-array/
 
-// Solution 1: Adding elements of second matrix into first one and then sorting
+// Solution 1: AUsing an extra space of O(m+n)
 
 class Solution {
 public:
@@ -17,4 +17,45 @@ public:
     }
 };
 
-// Solution 2:
+// Solution 2: Without using extra space
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+       if(n==0)
+           return;
+        if(nums1.size()==1 and n==1)
+            nums1[0]=nums2[0];
+        
+        for(int i=0;i<m;i++)
+        {
+            if(nums1[i]>nums2[0])
+            {
+                int t=nums1[i];
+                nums1[i]=nums2[0];
+                nums2[0]=t;
+            }
+            
+//             sorting to insert the first element at it's correct place
+            for(int j=0;j<n-1;j++)
+            {
+                if(nums2[j]>nums2[j+1])
+                {
+                    int t=nums2[j];
+                    nums2[j]=nums2[j+1];
+                    nums2[j+1]=t;
+                }
+                else
+                    break;
+            }
+        }
+        
+//         merging arrays
+        int j=0;
+        for(int i=m;i<m+n;i++)
+        {
+            nums1[i]=nums2[j];
+            j++;
+        }
+    }
+};
