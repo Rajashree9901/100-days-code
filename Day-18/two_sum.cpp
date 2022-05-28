a959b62d2169f031a732072b402d101f52b00966
Rajashree9901 <58505906+Rajashree9901@users.noreply.github.com>
2022-05-28 22:54:34 +0530

    Create two_sum.cpp

---
+++ Day-18/two_sum.cpp
@@ -0,0 +1,26 @@
+// Problem link- https://leetcode.com/problems/two-sum/
+
+// Solution -
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> v;
+        map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m.find(target-nums[i])==m.end())
+            {
+                m[nums[i]]=i;
+            }
+            else
+            {
+                v.push_back(m[target-nums[i]]);
+                v.push_back(i);
+                break;
+            }
+        }
+        return v;
+        
+    }
+};
